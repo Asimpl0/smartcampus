163acdfece5ac4f3c2aecdba0a2b1f3affedd2dd
jan <15271570344>
2022-04-13 00:01:53 +0800

    初步完成课表查询

--- pages/course/course.js
+++ pages/course/course.js
@@ -3,29 +3,40 @@
 var app = getApp()
 Page({
   data: {
-    colorArrays: [ "#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
-    wlist: [
-      { "xqj": 1, "skjc": 1, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "xqj": 1, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "xqj": 2, "skjc": 1, "skcd": 2,"kcmc":"高等数学@教A-301"},
-      { "xqj": 2, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "xqj": 3, "skjc": 4, "skcd": 1, "kcmc": "高等数学@教A-301" },
-      { "xqj": 3, "skjc": 8, "skcd": 1, "kcmc": "高等数学@教A-301" },
-      { "xqj": 3, "skjc": 5, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "xqj": 4, "skjc": 2, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "xqj": 4, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "xqj": 5, "skjc": 1, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "xqj": 6, "skjc": 3, "skcd": 2, "kcmc": "高等数学@教A-301" },
-
-      { "xqj": 7, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" },
-
-
-
-
-     
-    ]
+    courses:[""],
+    colorArrays: [ "#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"]
   },
   onLoad: function () {
-    console.log('onLoad')
+this.showCourse();
+    
+  },
+  showCourse:function() {
+    var header;
+    header = {
+      'content-type': 'application/x-www-form-urlencoded',
+    };
+    //从本地读取之前获得的cookie
+    var cookie = wx.getStorageSync('cookieKey'); //取出Cookie
+    //若存在cookie，加入请求
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    //console.log(cookie)
+    var URL = app.globalData.url + "course";
+    //console.log("login")
+    wx.request({
+      url: URL,
+      method: "GET",
+      header: header,
+      success:(res)=>{
+        var result = res.data
+        console.log(result)
+        this.setData({
+          courses: res.data
+        })
+        console.log(this.data.courses)
+      }
+      
+    })
   }
 })
