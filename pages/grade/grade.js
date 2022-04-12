9296126a2f34da6fa627ed0c40f5fbbc1fa3c7b5
jan <15271570344>
2022-04-12 11:37:16 +0800

    完成成绩查询

--- pages/grade/grade.js
+++ pages/grade/grade.js
@@ -6,88 +6,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-    stuId: "",
-    password: "",
-    jsonContent: '',
-    PicURL: "",
-    PicArr: [""],
-    hasUserInfo: false,
-    isLoading: true,
-    showGraphic: true,
-    painting: {},
-    shareImage: '',
-    isLoading: true,
-    enteryear:2018,
-    year:2018,
-    semester:0 //0表示总学年 1表示第一学期 2表示第二学期
+    isLoading: false,
+    enteryear: 2018,
+    year: 2018,
+    semester: 0 ,//0表示总学年 1表示第一学期 2表示第二学期,
+    scores:[""]
   },
   /**
    * 生命周期函数--监听页面加载   
    */
   onLoad: function (options) {
-    //获取成绩信息
-    this.GetScoreData();
+    this.showGrade();
   },
-  onChangeYear: function(event){
+  onChangeYear: function (event) {
     this.setData({
-      year:this.data.enteryear+event.detail.name
+      year: this.data.enteryear + event.detail.name
     })
     console.log(this.data.year)
     this.showGrade()
   },
-  onChangeSemester: function(event){
+  onChangeSemester: function (event) {
     this.setData({
-      semester:event.detail.name
+      semester: event.detail.name
     })
     console.log(this.data.semester)
     this.showGrade()
   },
   //展示成绩
-  showGrade:function(){
+  showGrade: function () {
+    var header;
+    header = {
+      'content-type': 'application/x-www-form-urlencoded',
+    };
+    //从本地读取之前获得的cookie
+    var cookie = wx.getStorageSync('cookieKey'); //取出Cookie
+    //若存在cookie，加入请求
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    //console.log(cookie)
+    var URL = app.globalData.url + "grade" +
+    "?year=" + this.data.year + "&semester=" + this.data.semester;
+    //console.log("login")
+    wx.request({
+      url: URL,
+      method: "GET",
+      header: header,
+      success:(res)=>{
+        var result = res.data
+        console.log(result)
+        this.setData({
+          scores: res.data
+        })
+        console.log(this.data.scores)
+      }
+      
+    })
     this.onShow();
   },
-  /**
-   * 查询成绩
-   */
-  GetScoreData: function () {
-    //用户记录都存储在缓存中，加快了打开成绩和教务网的速度，减少按量付费服务器的计费
-    let user = wx.getStorageSync('user');
-    let score_data = user.score_data
 
-    if (!score_data) {
-      let user = wx.getStorageSync('user');
-      this.setData({
-        isLoading: false,
-        jsonContent: user.scores
-      })
-    } else {
-      this.setData({
-        jsonContent: score_data,
-        isLoading: false
-      })
-      wx.showToast({
-        title: "更新完成",
-        icon: "succeed",
-        duration: 2000
-      })
-      return
-    }
-
-
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
     var that = this;
-    that.onLoad();
+    that.showGrade();
     wx.stopPullDownRefresh();
     wx.showToast({
       title: "更新完成",
       icon: "succeed",
-      duration: 2000
+      duration: 1000
     })
   },
 
@@ -96,16 +85,5 @@ Page({
    */
   onReachBottom: function () {
 
-  },
-
-
-  touchHandler: function (e) {
-    // console.log(lineChart.getCurrentDataIndex(e));
-    lineChart.showToolTip(e, {
-      // background: '#7cb5ec',
-      format: function (item, category) {
-        return category + ' ' + item.name + ':' + item.data
-      }
-    });
   }
 })
