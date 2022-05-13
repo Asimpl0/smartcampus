7b87906d649fcdcb0abc1c9d53ca9cfc1fc3b9a4
jan <15271570344>
2022-05-13 16:53:18 +0800

    完成课程搜索功能

--- pages/course/course.js
+++ pages/course/course.js
@@ -3,14 +3,13 @@
 var app = getApp()
 Page({
   data: {
-    courses:[""],
-    colorArrays: [ "#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"]
+    active: 0,
+    search: '',
+    courses: [""],
+    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
+    searchCourses:[]
   },
-  onLoad: function () {
-this.showCourse();
-    
-  },
-  showCourse:function() {
+  onSearch(){
     var header;
     header = {
       'content-type': 'application/x-www-form-urlencoded',
@@ -22,13 +21,56 @@ this.showCourse();
       header.Cookie = cookie;
     }
     //console.log(cookie)
-    var URL = app.globalData.url + "course";
+    var URL = app.globalData.url + "course?funct=1&search=" + this.data.search ;
     //console.log("login")
     wx.request({
       url: URL,
       method: "GET",
       header: header,
-      success:(res)=>{
+      success: (res) => {
+        console.log(res.data)
+        this.setData({
+          searchCourses:res.data
+        })
+      }
+
+    })
+  },
+  onCancel() {
+    this.setData({
+      search: ''
+    })
+    this.onSearch()
+  },
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({
+      active: event.detail
+    });
+  },
+  onLoad: function () {
+    this.showCourse();
+    this.onSearch()
+  },
+  showCourse: function () {
+    var header;
+    header = {
+      'content-type': 'application/x-www-form-urlencoded',
+    };
+    //从本地读取之前获得的cookie
+    var cookie = wx.getStorageSync('cookieKey'); //取出Cookie
+    //若存在cookie，加入请求
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    //console.log(cookie)
+    var URL = app.globalData.url + "course?funct=0";
+    //console.log("login")
+    wx.request({
+      url: URL,
+      method: "GET",
+      header: header,
+      success: (res) => {
         var result = res.data
         console.log(result)
         this.setData({
@@ -36,10 +78,10 @@ this.showCourse();
         })
         console.log(this.data.courses)
       }
-      
+
     })
   },
-  showCardView:function(){
-    
+  showCardView: function () {
+
   }
 })
